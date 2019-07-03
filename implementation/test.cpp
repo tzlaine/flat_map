6a2459c09e8f1936c4740862bc88db109cf4e0b3
Zach Laine <whatwasthataddress@gmail.com>
2019-07-02 21:59:17 -0500

    Impl: Add tests for flat_map_iterator.

--- implementation/test.cpp
+++ implementation/test.cpp
@@ -5,6 +5,165 @@
 #include <string>
 
 
+TEST(std_flat_map, iterator)
+{
+    using fmap_t = std::flat_map<std::string, int>;
+
+    {
+        fmap_t map;
+        fmap_t::iterator mutable_first = map.begin();
+        fmap_t::const_iterator const_first = mutable_first;
+
+        EXPECT_EQ(mutable_first, const_first);
+    }
+
+    {
+        fmap_t::containers c = {{"key0", "key1", "key2"}, {0, 1, 2}};
+
+        fmap_t::iterator const first(c.keys.begin(), c.values.begin());
+        fmap_t::iterator const last(c.keys.end(), c.values.end());
+
+        EXPECT_EQ(first + 3, last);
+        EXPECT_EQ(first, last - 3);
+
+        EXPECT_EQ(first[1].first, "key1");
+        EXPECT_EQ(last[-3].second, 0);
+
+        EXPECT_EQ((*(first + 1)).first, "key1");
+        EXPECT_EQ((*(last - 3)).second, 0);
+
+        EXPECT_EQ((first + 1)->first, "key1");
+        EXPECT_EQ((last - 3)->second, 0);
+
+        EXPECT_EQ(first - last, -3);
+        EXPECT_EQ(last - first, 3);
+
+        {
+            auto first_copy = first;
+            auto last_copy = last;
+
+            first_copy += 3;
+            last_copy -= 3;
+
+            EXPECT_EQ(first_copy, last);
+            EXPECT_EQ(last_copy, first);
+        }
+
+        {
+            auto first_copy = first;
+            auto last_copy = last;
+
+            EXPECT_EQ(first_copy++, first);
+            EXPECT_EQ(first_copy++, first + 1);
+            EXPECT_EQ(first_copy++, first + 2);
+            EXPECT_EQ(first_copy, last);
+
+            EXPECT_EQ(last_copy--, last);
+            EXPECT_EQ(last_copy--, last - 1);
+            EXPECT_EQ(last_copy--, last - 2);
+            EXPECT_EQ(last_copy, first);
+        }
+
+        {
+            auto first_copy = first;
+            auto last_copy = last;
+
+            EXPECT_EQ(++first_copy, first + 1);
+            EXPECT_EQ(++first_copy, first + 2);
+            EXPECT_EQ(++first_copy, last);
+            EXPECT_EQ(first_copy, last);
+
+            EXPECT_EQ(--last_copy, last - 1);
+            EXPECT_EQ(--last_copy, last - 2);
+            EXPECT_EQ(--last_copy, first);
+            EXPECT_EQ(last_copy, first);
+        }
+
+        EXPECT_EQ(first, first);
+        EXPECT_NE(first, last);
+
+        EXPECT_LT(first, last);
+        EXPECT_LE(first, last);
+        EXPECT_LE(first, first);
+        EXPECT_GE(last, first);
+        EXPECT_GE(first, first);
+        EXPECT_GT(last, first);
+    }
+
+    {
+        fmap_t::containers c = {{"key0", "key1", "key2"}, {0, 1, 2}};
+
+        fmap_t::reverse_iterator const first(c.keys.rbegin(), c.values.rbegin());
+        fmap_t::reverse_iterator const last(c.keys.rend(), c.values.rend());
+
+        EXPECT_EQ(first + 3, last);
+        EXPECT_EQ(first, last - 3);
+
+        EXPECT_EQ(first[1].first, "key1");
+        EXPECT_EQ(last[-3].second, 2);
+
+        EXPECT_EQ((*(first + 1)).first, "key1");
+        EXPECT_EQ((*(last - 3)).second, 2);
+
+        EXPECT_EQ((first + 1)->first, "key1");
+        EXPECT_EQ((last - 3)->second, 2);
+
+        EXPECT_EQ(first - last, -3);
+        EXPECT_EQ(last - first, 3);
+
+        {
+            auto first_copy = first;
+            auto last_copy = last;
+
+            first_copy += 3;
+            last_copy -= 3;
+
+            EXPECT_EQ(first_copy, last);
+            EXPECT_EQ(last_copy, first);
+        }
+
+        {
+            auto first_copy = first;
+            auto last_copy = last;
+
+            EXPECT_EQ(first_copy++, first);
+            EXPECT_EQ(first_copy++, first + 1);
+            EXPECT_EQ(first_copy++, first + 2);
+            EXPECT_EQ(first_copy, last);
+
+            EXPECT_EQ(last_copy--, last);
+            EXPECT_EQ(last_copy--, last - 1);
+            EXPECT_EQ(last_copy--, last - 2);
+            EXPECT_EQ(last_copy, first);
+        }
+
+        {
+            auto first_copy = first;
+            auto last_copy = last;
+
+            EXPECT_EQ(++first_copy, first + 1);
+            EXPECT_EQ(++first_copy, first + 2);
+            EXPECT_EQ(++first_copy, last);
+            EXPECT_EQ(first_copy, last);
+
+            EXPECT_EQ(--last_copy, last - 1);
+            EXPECT_EQ(--last_copy, last - 2);
+            EXPECT_EQ(--last_copy, first);
+            EXPECT_EQ(last_copy, first);
+        }
+
+        EXPECT_EQ(first, first);
+        EXPECT_NE(first, last);
+
+        EXPECT_LT(first, last);
+        EXPECT_LE(first, last);
+        EXPECT_LE(first, first);
+        EXPECT_GE(last, first);
+        EXPECT_GE(first, first);
+        EXPECT_GT(last, first);
+    }
+}
+
 TEST(std_flat_map, ctors)
 {
     using fmap_t = std::flat_map<std::string, int>;
@@ -13,6 +172,12 @@ TEST(std_flat_map, ctors)
     auto pair_cmp = [](auto lhs, auto rhs) {
         return lhs.first == rhs.first && lhs.second == rhs.second;
     };
+
+    {
+        fmap_t map;
+        EXPECT_TRUE(map.empty());
+        EXPECT_EQ(map.begin(), map.end());
+    }
 
     {
         std::vector<pair_t> const input_vec = {{"key1", 1}, {"key0", 0}};
