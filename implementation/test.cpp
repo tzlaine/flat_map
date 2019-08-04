5f96c5a4b516e94d2dc60449bf8a18abbb04e59d
Zach Laine <whatwasthataddress@gmail.com>
2019-08-04 15:08:36 -0500

    Add implementations for the allocator ctors, and add proper constraints throughout.

--- implementation/test.cpp
+++ implementation/test.cpp
@@ -188,6 +188,12 @@ TEST(std_flat_map, ctors_iterators)
     EXPECT_EQ(map_from_c, map_from_vec);
     EXPECT_FALSE(map_from_c != map_from_vec);
 
+    fmap_t const map_from_const_map = map_from_c;
+    fmap_t const map_from_map_rvalue = fmap_t(map_from_c);
+
+    EXPECT_EQ(map_from_const_map, map_from_vec);
+    EXPECT_FALSE(map_from_map_rvalue != map_from_vec);
+
     {
         fmap_t map;
         EXPECT_TRUE(map.empty());
@@ -208,13 +214,23 @@ TEST(std_flat_map, ctors_iterators)
     }
 
     {
-        // NOTE: This also exercises:
-        // template<class InputIterator>
-        // flat_map(
-        //     InputIterator first,
-        //     InputIterator last,
-        //     const key_compare & comp = key_compare());
-        
+        fmap_t map(vec.begin(), vec.end());
+        EXPECT_TRUE(std::equal(
+            map.begin(),
+            map.end(),
+            sorted_vec.begin(),
+            sorted_vec.end(),
+            pair_cmp));
+
+        EXPECT_TRUE(std::equal(
+            map.cbegin(),
+            map.cend(),
+            sorted_vec.begin(),
+            sorted_vec.end(),
+            pair_cmp));
+    }
+
+    {
         fmap_t map(vec);
         EXPECT_TRUE(std::equal(
             map.begin(),
@@ -260,14 +276,15 @@ TEST(std_flat_map, ctors_iterators)
     }
 
     {
-        // NOTE: This also exercises:
-        // template<class InputIterator>
-        // flat_map(
-        //     sorted_unique_t s,
-        //     InputIterator first,
-        //     InputIterator last,
-        //     const key_compare & comp = key_compare());
-        
+        // Broken contract, as above.
+        fmap_t map(std::sorted_unique, vec.begin(), vec.end());
+        EXPECT_EQ(map.size(), 3u);
+        EXPECT_FALSE(map.empty());
+        EXPECT_TRUE(std::equal(
+            map.begin(), map.end(), vec.begin(), vec.end(), pair_cmp));
+    }
+
+    {
         // Broken contract, as above.
         fmap_t map(std::sorted_unique, vec);
         EXPECT_EQ(map.size(), 3u);
@@ -306,6 +323,178 @@ TEST(std_flat_map, ctors_iterators)
 
     {
         fmap_t map;
+        map = {pair_t{"key1", 1}, pair_t{"key2", 2}, pair_t{"key0", 0}};
+        EXPECT_TRUE(std::equal(
+            map.begin(),
+            map.end(),
+            sorted_vec.begin(),
+            sorted_vec.end(),
+            pair_cmp));
+    }
+}
+
+TEST(std_flat_map, ctors_allocators_iterators)
+{
+    using fmap_t = std::flat_map<std::string, int>;
+    using pair_t = std::pair<std::string, int>;
+
+    auto pair_cmp = [](auto lhs, auto rhs) {
+        return lhs.first == rhs.first && lhs.second == rhs.second;
+    };
+
+    std::initializer_list<pair_t> init_list = {
+        {"key1", 1}, {"key2", 2}, {"key0", 0}};
+    fmap_t::containers const c = {{"key1", "key2", "key0"}, {1, 2, 0}};
+    std::vector<pair_t> const vec = {{"key1", 1}, {"key2", 2}, {"key0", 0}};
+    std::vector<pair_t> sorted_vec = vec;
+    std::sort(sorted_vec.begin(), sorted_vec.end());
+
+    auto allocator = c.keys.get_allocator();
+
+    fmap_t const map_from_c(c.keys, c.values, allocator);
+    fmap_t const map_from_vec(vec, allocator);
+
+    EXPECT_EQ(map_from_c, map_from_vec);
+    EXPECT_FALSE(map_from_c != map_from_vec);
+
+
+    fmap_t const map_from_const_map(map_from_c, allocator);
+    fmap_t const map_from_map_rvalue(fmap_t(map_from_c), allocator);
+
+    EXPECT_EQ(map_from_const_map, map_from_vec);
+    EXPECT_FALSE(map_from_map_rvalue != map_from_vec);
+
+
+    {
+        fmap_t map(allocator);
+        EXPECT_TRUE(map.empty());
+        EXPECT_EQ(map.size(), 0u);
+        EXPECT_EQ(map.begin(), map.end());
+    }
+
+    {
+        fmap_t map(c.keys, c.values, allocator);
+        EXPECT_EQ(map.size(), 3u);
+        EXPECT_FALSE(map.empty());
+        EXPECT_TRUE(std::equal(
+            map.begin(),
+            map.end(),
+            sorted_vec.begin(),
+            sorted_vec.end(),
+            pair_cmp));
+    }
+
+    {
+        fmap_t map(vec.begin(), vec.end(), allocator);
+        EXPECT_TRUE(std::equal(
+            map.begin(),
+            map.end(),
+            sorted_vec.begin(),
+            sorted_vec.end(),
+            pair_cmp));
+
+        EXPECT_TRUE(std::equal(
+            map.cbegin(),
+            map.cend(),
+            sorted_vec.begin(),
+            sorted_vec.end(),
+            pair_cmp));
+    }
+
+    {
+        fmap_t map(vec, allocator);
+        EXPECT_TRUE(std::equal(
+            map.begin(),
+            map.end(),
+            sorted_vec.begin(),
+            sorted_vec.end(),
+            pair_cmp));
+
+        EXPECT_TRUE(std::equal(
+            map.cbegin(),
+            map.cend(),
+            sorted_vec.begin(),
+            sorted_vec.end(),
+            pair_cmp));
+    }
+
+    {
+        std::flat_map<std::string, int, std::greater<std::string>> map(vec, allocator);
+        EXPECT_TRUE(std::equal(
+            map.rbegin(),
+            map.rend(),
+            sorted_vec.begin(),
+            sorted_vec.end(),
+            pair_cmp));
+
+        EXPECT_TRUE(std::equal(
+            map.crbegin(),
+            map.crend(),
+            sorted_vec.begin(),
+            sorted_vec.end(),
+            pair_cmp));
+    }
+
+    {
+        // This breaks the ctor's contract, since c.keys are not insorted
+        // order, but it's useful to verify that the resulting contents were
+        // not touched.
+        fmap_t map(std::sorted_unique, c.keys, c.values, allocator);
+        EXPECT_EQ(map.size(), 3u);
+        EXPECT_FALSE(map.empty());
+        EXPECT_TRUE(std::equal(
+            map.begin(), map.end(), vec.begin(), vec.end(), pair_cmp));
+    }
+
+    {
+        // Broken contract, as above.
+        fmap_t map(std::sorted_unique, vec.begin(), vec.end(), allocator);
+        EXPECT_EQ(map.size(), 3u);
+        EXPECT_FALSE(map.empty());
+        EXPECT_TRUE(std::equal(
+            map.begin(), map.end(), vec.begin(), vec.end(), pair_cmp));
+    }
+
+    {
+        // Broken contract, as above.
+        fmap_t map(std::sorted_unique, vec, allocator);
+        EXPECT_EQ(map.size(), 3u);
+        EXPECT_FALSE(map.empty());
+        EXPECT_TRUE(std::equal(
+            map.begin(), map.end(), vec.begin(), vec.end(), pair_cmp));
+    }
+
+    {
+        std::flat_map<std::string, int, std::greater<std::string>> map(
+            allocator);
+        map.insert(vec.begin(), vec.end());
+        EXPECT_TRUE(std::equal(
+            map.rbegin(),
+            map.rend(),
+            sorted_vec.begin(),
+            sorted_vec.end(),
+            pair_cmp));
+    }
+
+    {
+        fmap_t map(init_list, allocator);
+        EXPECT_TRUE(std::equal(
+            map.begin(),
+            map.end(),
+            sorted_vec.begin(),
+            sorted_vec.end(),
+            pair_cmp));
+    }
+
+    {
+        // Broken contract, as above.
+        fmap_t map(std::sorted_unique, init_list, allocator);
+        EXPECT_TRUE(std::equal(
+            map.begin(), map.end(), vec.begin(), vec.end(), pair_cmp));
+    }
+
+    {
+        fmap_t map(allocator);
         map = {pair_t{"key1", 1}, pair_t{"key2", 2}, pair_t{"key0", 0}};
         EXPECT_TRUE(std::equal(
             map.begin(),
@@ -357,8 +546,6 @@ TEST(std_flat_map, emplace_insert)
         return lhs.first == rhs.first && lhs.second == rhs.second;
     };
 
-    std::initializer_list<pair_t> init_list = {
-        {"key1", 1}, {"key2", 2}, {"key0", 0}};
     fmap_t::containers const c = {{"key1", "key2", "key0"}, {1, 2, 0}};
     std::vector<pair_t> const vec = {{"key1", 1}, {"key2", 2}, {"key0", 0}};
     std::vector<pair_t> sorted_vec = vec;
